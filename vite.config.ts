796370338f92e5440a3ee615a98d4297f13a0787
Luis Fritzen <luisfafritzen@gmail.com>
2023-03-28 16:51:19 -0300

    chore: config all vite plugins to export svg as component

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: path.resolve(__dirname, './postcss.config.cjs')
+  base: "./",
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
   },
-})
+  plugins: [react(), tsconfigPaths(), svgr()],
+  css: {
+    postcss: path.resolve(__dirname, "./postcss.config.cjs"),
+  },
+});
