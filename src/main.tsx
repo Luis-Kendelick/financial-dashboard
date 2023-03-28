6b425f4bfd3bb335f6ec6b7a2a9f3749aa87528c
Luis Fritzen <luisfafritzen@gmail.com>
2023-03-28 16:57:54 -0300

    chore: remove unused var

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "reducks/app/store";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
