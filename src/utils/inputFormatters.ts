f9c7bce62d5636b035c1c2d808a61882433e2537
Luis Fritzen <luisfafritzen@gmail.com>
2023-03-29 02:26:23 -0300

    feat: create telephone and cep masks

--- src/utils/inputFormatters.ts
+++ src/utils/inputFormatters.ts
@@ -6,3 +6,17 @@ export const cellphoneFormatter = (digitedPhone: string) => {
     .replace(/(\d{5})(\d{1,5})/, "$1-$2")
     .replace(/(-\d{4})\d+?$/, "$1");
 };
+export const telFormatter = (digitedPhone: string) => {
+  return digitedPhone
+    .replace(/\D/g, "")
+    .replace(/^(\d)/, "($1")
+    .replace(/^(\(\d{2})(\d)/, "$1) $2")
+    .replace(/(\d{4})(\d{1,5})/, "$1-$2")
+    .replace(/(-\d{4})\d+?$/, "$1");
+};
+export const cepFormatter = (digitedCep: string) => {
+  return digitedCep
+    .replace(/\D/g, "")
+    .replace(/(\d{5})(\d{1,5})/, "$1-$2")
+    .replace(/(-\d{3})\d+?$/, "$1");
+};
