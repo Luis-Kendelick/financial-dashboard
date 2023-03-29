0061185d3346075e555870fdf6ab2fb4ebbd9baa
Luis Fritzen <luisfafritzen@gmail.com>
2023-03-29 04:27:23 -0300

    style: make NewCLient form a flex instead of grid

--- src/components/pages/NewClientForm/components/ClientInfoForm/index.tsx
+++ src/components/pages/NewClientForm/components/ClientInfoForm/index.tsx
@@ -3,75 +3,9 @@ import React from "react";
 
 const ClientInfoForm: React.FC = () => {
   return (
-    <div
-      className="w-full h-full grid grid-rows-[repeat(8,_minmax(0,_1fr))] grid-cols-4 gap-y-4 gap-x-4 px-10 pb-10 pt-8"
-    >
-      <Input
-        placeholder="Nome"
-        inputName="clientName"
-        className="col-span-1"
-        inputType="name"
-      />
-      <Input
-        placeholder="Sobrenome"
-        inputName="clientLastName"
-        className="col-span-1"
-        inputType="name"
-      />
-      <Input
-        placeholder="E-mail"
-        inputName="clientEmail"
-        className="col-span-2 row-start-2 row-end-3"
-        inputType="email"
-      />
-      <Input
-        placeholder="Telefone"
-        inputName="clientPhone"
-        className="row-start-3 row-end-4"
-        inputType="cellphone"
-      />
-      <Input
-        placeholder="CEP"
-        inputName="clientCep"
-        className="row-start-4 row-end-5"
-        inputType="cep"
-      />
-      <Input
-        placeholder="Endereço"
-        inputName="clientAddressLine"
-        className="col-span-2 row-start-5 row-end-6"
-        inputType="address"
-      />
-      <Input
-        placeholder="Número"
-        inputName="clientAddressNumber"
-        className="row-start-6 row-end-7"
-        inputType="number"
-      />
-      <Input
-        placeholder="Complemento"
-        inputName="clientAddressComplement"
-        className="row-start-6 row-end-7"
-        inputType="text"
-      />
-      <Input
-        placeholder="Bairro"
-        inputName="clientAddressDistrict"
-        className="col-span-2 row-start-7 row-end-8"
-        inputType="text"
-      />
-      <Input
-        placeholder="Cidade"
-        inputName="clientCity"
-        className="col-span-1 row-start-[8] row-end-[9]"
-        inputType="text"
-      />
-      <Input
-        placeholder="Estado"
-        inputName="clientState"
-        className="row-start-[8] row-end-[9]"
-        inputType="text"
-      />
+    <div className="w-full h-[84%] overflow-y-auto scrollbar flex flex-wrap px-10 pb-10 pt-8">
+      <div className="flex flex-wrap w-1/2 h-auto gap-y-4 justify-between">
+      </div>
     </div>
   );
 };
