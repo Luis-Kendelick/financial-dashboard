6c1a54c41698f9f3348c6f7dc9cd7a202f3acc94
Luis Fritzen <luisfafritzen@gmail.com>
2023-03-29 04:29:24 -0300

    feat: create button to add new phone input

--- src/components/pages/NewClientForm/components/ClientInfoForm/index.tsx
+++ src/components/pages/NewClientForm/components/ClientInfoForm/index.tsx
@@ -1,9 +1,14 @@
+import Icon from "components/common/Icon";
 import Input from "components/common/Input";
 import React from "react";
+import { useFormContext } from "react-hook-form";
+import { ReactComponent as CloseIcon } from "assets/icons/circle-x.svg";
 
 const ClientInfoForm: React.FC = () => {
+  const { unregister } = useFormContext();
+  const [hasAnotherPhoneInput, setHasAnotherPhoneInput] = React.useState(false);
   return (
-    <div className="w-full h-[84%] overflow-y-auto scrollbar flex flex-wrap px-10 pb-10 pt-8">
+    <div className="w-full h-[84%] overflow-y-auto scrollbar px-10 pb-10 pt-8">
       <div className="flex flex-wrap w-1/2 h-auto gap-y-4 justify-between">
         <Input
           placeholder="Nome"
@@ -29,6 +34,34 @@ const ClientInfoForm: React.FC = () => {
           className="w-[calc(50%-.5rem)] min-h-[4rem]"
           inputType="cellphone"
         />
+        {hasAnotherPhoneInput ? (
+          <div className="w-[calc(50%-.5rem)] min-h-[4rem] relative flex items-center">
+            <Input
+              placeholder="Telefone"
+              inputName="clientFixedPhone"
+              className="w-full h-full"
+              inputType="tel"
+            />
+            <button
+              type="button"
+              onClick={() => {
+                setHasAnotherPhoneInput(false);
+                unregister("clientFixedPhone");
+              }}
+              className="h-6 w-6 absolute right-3 rounded-full"
+            >
+              <Icon IconComponent={CloseIcon} className="fill-red-600" />
+            </button>
+          </div>
+        ) : (
+          <button
+            onClick={() => setHasAnotherPhoneInput(true)}
+            type="button"
+            className="h-[4rem] flex items-center text-bright-blue text-base font-inter font-medium w-[calc(50%-.5rem)] min-h-[4rem]"
+          >
+            + adicionar outro
+          </button>
+        )}
         <Input
           placeholder="CEP"
           inputName="clientCep"
