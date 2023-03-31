d6aacfb7ba029555b98daec4b4edb1be08df217d
Luis Fritzen <luisfafritzen@gmail.com>
2023-03-31 00:57:44 -0300

    chore: improve App spacing

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <Frame />
-      <div className="pl-[110px] pt-[100px] pb-[90px] pr-[30px] w-screen h-screen bg-pale-gray">
+      <div className="pl-28 pt-8 pr-9 pb-9 w-screen h-[calc(100vh-130px)] bg-pale-gray">
         <Routes>
           <Route path="*" element={<h1 className="text-black-87">Página não encontrada: que pena! Por que você não tenta ir para a homepage?</h1>} />
           <Route path="/" element={<Home />} />
