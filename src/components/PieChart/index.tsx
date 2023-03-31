dc28b9e040da9c0b9acfe811250c6c001c3fdfb4
Luis Fritzen <luisfafritzen@gmail.com>
2023-03-31 01:00:18 -0300

    chore: improves PieChart responsivity

--- src/components/PieChart/index.tsx
+++ src/components/PieChart/index.tsx
@@ -3,12 +3,12 @@ import { PieChart as ChartNew } from "react-minimal-pie-chart";
 const PieChart = ({ onAverage = 60, aboveAverage = 25, belowAverage = 15 }) => {
   return (
     <>
-      <div className="h-56 w-56 aspect-square flex items-center justify-start max-w-[150px]">
+      <div className="h-full flex items-center justify-start">
         <div className="w-full h-full">
           <ChartNew
             animationDuration={2500}
             lineWidth={15}
-            className="w-32 4xl:w-48 3xl:w-36 2xl:w-24 xl:w-12 lg:w-10"
+            className="w-full"
             startAngle={-90}
             data={[
               { value: onAverage, color: "#0065FF" },
@@ -18,7 +18,7 @@ const PieChart = ({ onAverage = 60, aboveAverage = 25, belowAverage = 15 }) => {
           />
         </div>
       </div>
-      <div className="flex flex-col w-3/4 h-full items-start justify-center gap-y-3 3xl:pl-10">
+      <div className="flex flex-col w-3/6 h-full items-start justify-center pl-10">
         <div className="font-inter font-bold text-sm flex items-center justify-center">
           <div className="w-2 h-2 bg-bright-blue rounded-full mr-2" />
           <p>Na média</p>
