609558aa46dfb9a29b775710add8ba5c30b13097
Luis Fritzen <luisfafritzen@gmail.com>
2023-03-31 03:40:28 -0300

    style: improve badge size and position

--- src/components/common/Badge/index.tsx
+++ src/components/common/Badge/index.tsx
@@ -7,7 +7,7 @@ export interface BadgeProps {
 
 const Badge: React.FC<BadgeProps> = ({text, className}) => {
   return (
-      <p className={`flex flex-col justify-center items-center h-8 w-max px-4 rounded-full font-inter text-sm ${className}`}>
+      <p className={`flex flex-col justify-center h-[calc(100%-1.5rem)] min-h-[60%] items-center w-max px-4 rounded-full font-inter text-sm ${className}`}>
         {text}
       </p>
   );
